0753c04c11f6f6d891eb7dea8f921b131a86d408
Damian Łączyński <damian.laczynski.edu@gmail.com>
2025-03-13 20:52:13 +0100

    Remove Deprecated Animation Provider from App Configuration
    - Eliminated the `provideAnimationsAsync` function from the app configuration to streamline the setup.
    - This change simplifies the application configuration by removing unused dependencies, enhancing maintainability.
    
    This commit improves the clarity and efficiency of the app configuration by removing deprecated elements.

--- src/KeepItUp.MagJob.Client/src/app.config.ts
+++ src/KeepItUp.MagJob.Client/src/app.config.ts
@@ -2,7 +2,6 @@ import { ApplicationConfig, provideAppInitializer, inject } from '@angular/core'
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideOAuthClient } from 'angular-oauth2-oidc';
 import { tokenInterceptor } from './app/core/interceptors/token.interceptor';
 import { AuthService } from './app/core/services/auth.service';
@@ -16,6 +15,5 @@ export const appConfig: ApplicationConfig = {
       const authService = inject(AuthService);
       return authService.initAuth();
     }),
-    provideAnimationsAsync('noop'),
   ],
 };
